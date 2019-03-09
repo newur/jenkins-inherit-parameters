f9b1fc50f605a3b47dd6a8e88eadd1fdaa860a0d
Ruwen Reddig <ruwen.reddig@gmail.com>
2019-03-09 10:53:35 +0100

    add global var

--- vars/updateParams.groovy
+++ vars/updateParams.groovy
@@ -1,7 +1,9 @@
 def call(script) {
     println script.metaClass.methods.name.unique()
-    println script.dump()
-    println Arrays.asList(script.getClass().getInterfaces())
-    println Arrays.asList(script.getClass().getDeclaredFields())
-    println Arrays.asList(script.getClass().getDeclaredMethods())
+//    println script.dump()
+//    println Arrays.asList(script.getClass().getInterfaces())
+//    println Arrays.asList(script.getClass().getDeclaredFields())
+//    println Arrays.asList(script.getClass().getDeclaredMethods())
+    println script.env.HOME
+    println script.params
 }
